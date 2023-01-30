9ea48f9137fdbf8a9bc0f8b393fb2d8fcea5d61b
Gabriel <gabriellagrota23@gmail.com>
2023-01-30 11:57:57 -0300

    build: Projeto buildado no spring initializer

---
+++ src/main/java/br/akd/svc/akadia/AkadiaApplication.java
@@ -0,0 +1,13 @@
+package br.akd.svc.akadia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkadiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkadiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/akd/svc/akadia/AkadiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.akd.svc.akadia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkadiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
