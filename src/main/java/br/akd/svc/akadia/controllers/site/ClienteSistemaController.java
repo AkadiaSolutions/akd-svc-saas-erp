5307b3091e8fd80a8f31554cfdf2473b64f8c84d
Gabriel <gabriellagrota23@gmail.com>
2023-02-02 08:54:39 -0300

    feat: Criados métodos controladores de validação de cpf (2° etapa de cadastro) e de finalização de cadastro (3° etapa de cadastro) do cliente do sistema

--- src/main/java/br/akd/svc/akadia/controllers/site/ClienteSistemaController.java
+++ src/main/java/br/akd/svc/akadia/controllers/site/ClienteSistemaController.java
@@ -1,8 +1,9 @@
 package br.akd.svc.akadia.controllers.site;
 
 import br.akd.svc.akadia.models.dto.site.ClienteSistemaDto;
-import br.akd.svc.akadia.models.dto.site.EmpresaDto;
+import br.akd.svc.akadia.models.entities.bckoff.LeadEntity;
 import br.akd.svc.akadia.models.entities.site.ClienteSistemaEntity;
+import br.akd.svc.akadia.services.bckoff.LeadService;
 import br.akd.svc.akadia.services.global.exceptions.InvalidRequestException;
 import br.akd.svc.akadia.services.site.ClienteSistemaService;
 import io.swagger.annotations.Api;
@@ -10,6 +11,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,29 +32,54 @@ public class ClienteSistemaController {
     @Autowired
     ClienteSistemaService clienteSistemaService;
 
+    @Autowired
+    LeadService leadService;
+
     @ApiOperation(
-            value = "Cadastro de novo cliente (1° Etapa)",
-            notes = "Essa requisição tem como objetivo realizar o cadastro das informações básicas do cliente no banco " +
-                    "de dados para formação de leads",
+            value = "Captação de lead da criação de novo cliente",
+            notes = "Esse endpoint tem como objetivo realizar o cadastro das informações básicas do cliente no banco " +
+                    "de dados para formação de leads, além de verificar se o e-mail informado já existe",
             produces = MediaType.APPLICATION_JSON,
             consumes = MediaType.APPLICATION_JSON
     )
     @ApiResponses({
-            @ApiResponse(code = 201, message = "Dados captados com sucesso", response = EmpresaDto.class),
-            @ApiResponse(code = 400, message = "Falha na criação do cliente", response = InvalidRequestException.class),
-            @ApiResponse(code = 401, message = "Falha de autenticação"),
+            @ApiResponse(code = 201, message = "Dados persistidos com sucesso", response = LeadEntity.class),
+            @ApiResponse(code = 400, message = "O e-mail informado já existe", response = InvalidRequestException.class),
     })
-    @PostMapping
-    public ResponseEntity<ClienteSistemaEntity> primeiraEtapaCadastro(@RequestBody ClienteSistemaDto clienteSistemaDto) {
-        return ResponseEntity.ok().body(clienteSistemaService.primeiraEtapaCadastroCliente(clienteSistemaDto));
+    @PostMapping("cadastro/verifica-email")
+    public ResponseEntity<LeadEntity> captaLeadsPreCadastro(@RequestBody ClienteSistemaDto clienteSistemaDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(leadService.encaminhaLeadParaPersistencia(clienteSistemaDto));
     }
 
+    @ApiOperation(
+            value = "Validação de CPF da criação de novo cliente",
+            notes = "Esse endpoint tem como objetivo realizar a verificação de já existência do CPF enviado",
+            produces = MediaType.APPLICATION_JSON,
+            consumes = MediaType.APPLICATION_JSON
+    )
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "O cpf informado ainda não existe", response = ClienteSistemaEntity.class),
+            @ApiResponse(code = 400, message = "O cpf informado já existe", response = InvalidRequestException.class),
+    })
+    @PostMapping("cadastro/verifica-cpf")
+    public ResponseEntity<ClienteSistemaEntity> verificaSeCpfJaExiste(@RequestBody ClienteSistemaDto clienteSistemaDto) {
+        clienteSistemaService.validaSeCpfJaExiste(clienteSistemaDto);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 
-
-
-
-
-
-
+    @ApiOperation(
+            value = "Cadastro de novo cliente",
+            notes = "Esse endpoint tem como objetivo realizar o cadastro de um novo cliente no banco de dados d projeto",
+            produces = MediaType.APPLICATION_JSON,
+            consumes = MediaType.APPLICATION_JSON
+    )
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cliente salvo com sucesso", response = ClienteSistemaEntity.class)
+            //TODO ADICIONAR RESPONSES DE ERRORS
+    })
+    @PostMapping("cadastro/cria-cliente")
+    public ResponseEntity<ClienteSistemaEntity> criaNovoCliente(@RequestBody ClienteSistemaDto clienteSistemaDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteSistemaService.cadastraNovoCliente(clienteSistemaDto));
+    }
 
 }
