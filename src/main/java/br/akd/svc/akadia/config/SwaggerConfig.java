eeeeccbdd7ed5105fe15636b4d5014166592c40a
Gabriel <gabriellagrota23@gmail.com>
2023-02-02 14:31:34 -0300

    feat: Swagger adicionado ao projeto

---
+++ src/main/java/br/akd/svc/akadia/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package br.akd.svc.akadia.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.akd.svc.akadia"))
+                .paths(regex("/api/v1.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        return new ApiInfo(
+                "Akadia Solutions",
+                "Back end do ecosistema Akadia",
+                "1.0.0",
+                "Terms of Service",
+                new Contact("Gabriel Lagrota",
+                        "https://github.com/AkadiaSolutions/akd-svc-saas-erp",
+                        "gabriellagrota23@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licensen.html",
+                new ArrayList<>()
+        );
+    }
+
+}
