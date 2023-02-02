57b244cd7819de5fa444ba3741e7af2a8fe06fa9
Gabriel <gabriellagrota23@gmail.com>
2023-02-02 14:31:58 -0300

    feat: @ComponentScan apontado para os packages do projeto

--- src/main/java/br/akd/svc/akadia/AkadiaApplication.java
+++ src/main/java/br/akd/svc/akadia/AkadiaApplication.java
@@ -2,10 +2,20 @@ package br.akd.svc.akadia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableFeignClients
 @SpringBootApplication
+@EntityScan(basePackages = {"br.akd.svc.akadia.models"})
+@EnableJpaRepositories("br.akd.svc.akadia.repositories")
+@ComponentScan("br.akd.svc.akadia.controllers")
+@ComponentScan("br.akd.svc.akadia.services")
+@ComponentScan("br.akd.svc.akadia.proxy")
+@ComponentScan("br.akd.svc.akadia.config")
+@ComponentScan("br.akd.svc.akadia.utils")
 public class AkadiaApplication {
 
 	public static void main(String[] args) {
