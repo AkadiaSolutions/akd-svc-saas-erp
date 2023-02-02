47b05103cec59982b474fa08f2773ba67164e90b
Gabriel <gabriellagrota23@gmail.com>
2023-02-02 11:11:48 -0300

    feat: Realizada otimização de código de cadastro de cliente no sistema e na integradora

--- src/main/java/br/akd/svc/akadia/services/site/ClienteSistemaService.java
+++ src/main/java/br/akd/svc/akadia/services/site/ClienteSistemaService.java
@@ -2,13 +2,16 @@ package br.akd.svc.akadia.services.site;
 
 import br.akd.svc.akadia.models.dto.site.ClienteSistemaDto;
 import br.akd.svc.akadia.models.entities.global.TelefoneEntity;
+import br.akd.svc.akadia.models.entities.site.CartaoEntity;
 import br.akd.svc.akadia.models.entities.site.ClienteSistemaEntity;
 import br.akd.svc.akadia.models.entities.site.PlanoEntity;
+import br.akd.svc.akadia.models.enums.site.FormaPagamentoSistemaEnum;
 import br.akd.svc.akadia.models.enums.site.StatusPlanoEnum;
 import br.akd.svc.akadia.proxy.asaas.AsaasProxy;
 import br.akd.svc.akadia.proxy.asaas.requests.ClienteSistemaRequest;
 import br.akd.svc.akadia.proxy.asaas.responses.ClienteSistemaResponse;
 import br.akd.svc.akadia.repositories.site.impl.ClienteSistemaRepositoryImpl;
+import br.akd.svc.akadia.services.global.exceptions.FeignConnectionException;
 import br.akd.svc.akadia.services.global.exceptions.InvalidRequestException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +21,6 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class ClienteSistemaService {
@@ -44,35 +45,53 @@ public class ClienteSistemaService {
 
     public ClienteSistemaEntity cadastraNovoCliente(ClienteSistemaDto clienteSistemaDto) {
 
-        ClienteSistemaEntity clienteSistema = ClienteSistemaEntity.builder()
-                .dataCadastro(LocalDate.now().toString())
-                .horaCadastro(LocalTime.now().toString())
-                .dataNascimento(clienteSistemaDto.getDataNascimento())
-                .email(clienteSistemaDto.getEmail())
-                .senha(clienteSistemaDto.getSenha())
-                .cpf(clienteSistemaDto.getCpf())
-                .nome(clienteSistemaDto.getNome())
-                .telefone(TelefoneEntity.builder()
-                        .prefixo(clienteSistemaDto.getTelefone().getPrefixo())
-                        .numero(clienteSistemaDto.getTelefone().getNumero())
-                        .tipoTelefoneEnum(clienteSistemaDto.getTelefone().getTipoTelefoneEnum())
-                        .build())
-                .saldo(0.00)
-                .codigoClienteAsaas(null)
-                .cartoes(new ArrayList<>())
-                .empresas(new ArrayList<>())
-                .endereco(null)
-                .pagamentos(new ArrayList<>())
-                .build();
+        ClienteSistemaEntity clienteSistema =
+                ClienteSistemaEntity.builder()
+                        .dataCadastro(LocalDate.now().toString())
+                        .horaCadastro(LocalTime.now().toString())
+                        .dataNascimento(clienteSistemaDto.getDataNascimento())
+                        .email(clienteSistemaDto.getEmail())
+                        .senha(clienteSistemaDto.getSenha())
+                        .cpf(clienteSistemaDto.getCpf())
+                        .nome(clienteSistemaDto.getNome())
+                        .telefone(TelefoneEntity.builder()
+                                .prefixo(clienteSistemaDto.getTelefone().getPrefixo())
+                                .numero(clienteSistemaDto.getTelefone().getNumero())
+                                .tipoTelefoneEnum(clienteSistemaDto.getTelefone().getTipoTelefoneEnum())
+                                .build())
+                        .saldo(0.00)
+                        .codigoClienteAsaas(null)
+                        .cartao(clienteSistemaDto.getPlano().getFormaPagamentoSistemaEnum().equals(FormaPagamentoSistemaEnum.CARTAO_CREDITO)
+                                ? CartaoEntity.builder()
+                                .cvv(clienteSistemaDto.getCartao().getCvv())
+                                .ativo(true)
+                                .bandeiraCartaoEnum(clienteSistemaDto.getCartao().getBandeiraCartaoEnum())
+                                .mesExpiracao(clienteSistemaDto.getCartao().getMesExpiracao())
+                                .anoExpiracao(clienteSistemaDto.getCartao().getAnoExpiracao())
+                                .numero(clienteSistemaDto.getCartao().getNumero())
+                                .cpfCnpj(clienteSistemaDto.getCartao().getCpfCnpj())
+                                .pagamentos(new ArrayList<>())
+                                .tokenCartao(null)
+                                .build()
+                                : null)
+                        .plano(PlanoEntity.builder()
+                                .dataContratacao(LocalDate.now().toString())
+                                .horaContratacao(LocalTime.now().toString())
+                                .tipoPlanoEnum(clienteSistemaDto.getPlano().getTipoPlanoEnum())
+                                .formaPagamentoSistemaEnum(clienteSistemaDto.getPlano().getFormaPagamentoSistemaEnum())
+                                .statusPlanoEnum(StatusPlanoEnum.PERIODO_DE_TESTES)
+                                .build())
+                        .endereco(null)
+                        .empresas(new ArrayList<>())
+                        .pagamentos(new ArrayList<>())
+                        .build();
 
-        PlanoEntity plano = PlanoEntity.builder()
-                .dataContratacao(LocalDate.now().toString())
-                .horaContratacao(LocalDate.now().toString())
-                .tipoPlanoEnum(clienteSistemaDto.getPlano().getTipoPlanoEnum())
-                .formaPagamentoSistemaEnum(clienteSistemaDto.getPlano().getFormaPagamentoSistemaEnum())
-                .statusPlanoEnum(StatusPlanoEnum.PERIODO_DE_TESTES)
-                .build();
-        clienteSistema.setPlano(plano);
+
+        clienteSistema.setCodigoClienteAsaas(realizaCadastroClienteAsaas(clienteSistema).getId());
+        return clienteSistemaRepositoryImpl.implementaPersistencia(clienteSistema);
+    }
+
+    private ClienteSistemaResponse realizaCadastroClienteAsaas(ClienteSistemaEntity clienteSistema) {
 
         ClienteSistemaRequest clienteSistemaRequest = ClienteSistemaRequest.builder()
                 .name(clienteSistema.getNome())
@@ -81,25 +100,23 @@ public class ClienteSistemaService {
                 .cpfCnpj(clienteSistema.getCpf())
                 .build();
 
-        Map<String, String> headers = new HashMap<>();
-        headers.put("access_token", System.getenv("TOKEN_ASAAS"));
+        ResponseEntity<?> cadastraClienteAsaas;
 
-        System.err.println(headers);
+        try {
+            cadastraClienteAsaas = asaasProxy.cadastraCliente(clienteSistemaRequest, System.getenv("TOKEN_ASAAS"));
+        } catch (Exception e) {
+            throw new FeignConnectionException("Ocorreu uma falha na comunicação com a integradora de pagamentos: " + e.getMessage());
+        }
 
-        ResponseEntity<?> cadastraClienteAsaas =
-                asaasProxy.cadastraCliente(clienteSistemaRequest, headers);
+        if (cadastraClienteAsaas.getStatusCodeValue() != 200)
+            throw new InvalidRequestException("Ocorreu um erro no processo de criação do cliente: "
+                    + cadastraClienteAsaas.getBody());
 
-        ClienteSistemaResponse clienteSistemaResponse =
-                objectMapper.convertValue(cadastraClienteAsaas.getBody(), ClienteSistemaResponse.class);
-
-        clienteSistema.setCodigoClienteAsaas(clienteSistemaResponse.getId());
-
-        System.err.println(cadastraClienteAsaas);
-        System.err.println(clienteSistemaResponse);
-
-        return clienteSistemaRepositoryImpl.implementaPersistencia(clienteSistema);
-
+        return objectMapper.convertValue(cadastraClienteAsaas.getBody(), ClienteSistemaResponse.class);
     }
 
+    private void criaAssinaturaClienteAsaas(ClienteSistemaEntity clienteSistema) {
+
+    }
 
 }
