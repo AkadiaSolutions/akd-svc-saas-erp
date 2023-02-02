d62d5dd3a8b4a47aa1cd57b02e2d7821bbf821e1
Gabriel <gabriellagrota23@gmail.com>
2023-02-02 08:53:58 -0300

    feat: Criados objetos ClienteSistemRequest e ClienteSistemaResponse, que tem como objetivo enviar e receber requisições pelo proxy da integradora de pagamentos

---
+++ src/main/java/br/akd/svc/akadia/proxy/asaas/requests/ClienteSistemaRequest.java
@@ -0,0 +1,26 @@
+package br.akd.svc.akadia.proxy.asaas.requests;
+
+import lombok.*;
+
+@Builder
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteSistemaRequest {
+    private String name;
+    private String email;
+    private String phone;
+    private String mobilePhone;
+    private String cpfCnpj;
+    private String postalCode;
+    private String address;
+    private String addressNumber;
+    private String complement;
+    private String province;
+    private String externalReference;
+    private Boolean notificationDisabled;
+    private String additionalEmails;
+    private String municipalInscription;
+    private String stateInscription;
+    private String observations;
+}
---
+++ src/main/java/br/akd/svc/akadia/proxy/asaas/responses/ClienteSistemaResponse.java
@@ -0,0 +1,42 @@
+package br.akd.svc.akadia.proxy.asaas.responses;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteSistemaResponse {
+    private String object;
+    private String id;
+    private String dateCreated;
+    private String name;
+    private String email;
+    private String company;
+    private String phone;
+    private String mobilePhone;
+    private String address;
+    private String addressNumber;
+    private String complement;
+    private String province;
+    private String postalCode;
+    private String cpfCnpj;
+    private String personType;
+    private Boolean deleted;
+    private String additionalEmails;
+    private String externalReference;
+    private Boolean notificationDisabled;
+    private String observations;
+    private String municipalInscription;
+    private String stateInscription;
+    private Boolean canDelete;
+    private String cannotBeDeletedReason;
+    private Boolean canEdit;
+    private String cannotEditReason;
+    private Boolean foreignCustomer;
+    private Integer city;
+    private String state;
+    private String country;
+
+}
