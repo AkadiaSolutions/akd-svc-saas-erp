86b2481a5fe17982b56737d74c1c1fd738222655
Gabriel <gabriellagrota23@gmail.com>
2023-02-02 08:54:59 -0300

    feat: Criados métodos de serviços de validação de cpf (2° etapa de cadastro) e de finalização de cadastro (3° etapa de cadastro) do cliente do sistema

--- src/main/java/br/akd/svc/akadia/services/site/ClienteSistemaService.java
+++ src/main/java/br/akd/svc/akadia/services/site/ClienteSistemaService.java
@@ -1,10 +1,25 @@
 package br.akd.svc.akadia.services.site;
 
 import br.akd.svc.akadia.models.dto.site.ClienteSistemaDto;
+import br.akd.svc.akadia.models.entities.global.TelefoneEntity;
 import br.akd.svc.akadia.models.entities.site.ClienteSistemaEntity;
+import br.akd.svc.akadia.models.entities.site.PlanoEntity;
+import br.akd.svc.akadia.models.enums.site.StatusPlanoEnum;
+import br.akd.svc.akadia.proxy.asaas.AsaasProxy;
+import br.akd.svc.akadia.proxy.asaas.requests.ClienteSistemaRequest;
+import br.akd.svc.akadia.proxy.asaas.responses.ClienteSistemaResponse;
 import br.akd.svc.akadia.repositories.site.impl.ClienteSistemaRepositoryImpl;
+import br.akd.svc.akadia.services.global.exceptions.InvalidRequestException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ClienteSistemaService {
@@ -12,16 +27,79 @@ public class ClienteSistemaService {
     @Autowired
     ClienteSistemaRepositoryImpl clienteSistemaRepositoryImpl;
 
-    public ClienteSistemaEntity primeiraEtapaCadastroCliente(ClienteSistemaDto clienteSistemaDto) {
+    @Autowired
+    AsaasProxy asaasProxy;
 
-        ClienteSistemaEntity clienteSistema = (
-                ClienteSistemaEntity.builder()
-                        .senha(clienteSistemaDto.getSenha())
-                        .email(clienteSistemaDto.getEmail())
-                        .build()
-        );
+    ObjectMapper objectMapper = new ObjectMapper();
 
-        return clienteSistemaRepositoryImpl.salvaNovoCliente(clienteSistema);
+    public void validaSeEmailJaExiste(ClienteSistemaDto clienteSistemaDto) {
+        if (clienteSistemaRepositoryImpl.implementaBuscaPorEmail(clienteSistemaDto.getEmail()).isPresent())
+            throw new InvalidRequestException("O e-mail informado já existe");
     }
+
+    public void validaSeCpfJaExiste(ClienteSistemaDto clienteSistemaDto) {
+        if (clienteSistemaRepositoryImpl.implementaBuscaPorCpf(clienteSistemaDto.getCpf()).isPresent())
+            throw new InvalidRequestException("O cpf informado já existe");
+    }
+
+    public ClienteSistemaEntity cadastraNovoCliente(ClienteSistemaDto clienteSistemaDto) {
+
+        ClienteSistemaEntity clienteSistema = ClienteSistemaEntity.builder()
+                .dataCadastro(LocalDate.now().toString())
+                .horaCadastro(LocalTime.now().toString())
+                .dataNascimento(clienteSistemaDto.getDataNascimento())
+                .email(clienteSistemaDto.getEmail())
+                .senha(clienteSistemaDto.getSenha())
+                .cpf(clienteSistemaDto.getCpf())
+                .nome(clienteSistemaDto.getNome())
+                .telefone(TelefoneEntity.builder()
+                        .prefixo(clienteSistemaDto.getTelefone().getPrefixo())
+                        .numero(clienteSistemaDto.getTelefone().getNumero())
+                        .tipoTelefoneEnum(clienteSistemaDto.getTelefone().getTipoTelefoneEnum())
+                        .build())
+                .saldo(0.00)
+                .codigoClienteAsaas(null)
+                .cartoes(new ArrayList<>())
+                .empresas(new ArrayList<>())
+                .endereco(null)
+                .pagamentos(new ArrayList<>())
+                .build();
+
+        PlanoEntity plano = PlanoEntity.builder()
+                .dataContratacao(LocalDate.now().toString())
+                .horaContratacao(LocalDate.now().toString())
+                .tipoPlanoEnum(clienteSistemaDto.getPlano().getTipoPlanoEnum())
+                .formaPagamentoSistemaEnum(clienteSistemaDto.getPlano().getFormaPagamentoSistemaEnum())
+                .statusPlanoEnum(StatusPlanoEnum.PERIODO_DE_TESTES)
+                .build();
+        clienteSistema.setPlano(plano);
+
+        ClienteSistemaRequest clienteSistemaRequest = ClienteSistemaRequest.builder()
+                .name(clienteSistema.getNome())
+                .email(clienteSistema.getEmail())
+                .phone(clienteSistema.getTelefone().getPrefixo().toString() + clienteSistema.getTelefone().getNumero())
+                .cpfCnpj(clienteSistema.getCpf())
+                .build();
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("access_token", System.getenv("TOKEN_ASAAS"));
+
+        System.err.println(headers);
+
+        ResponseEntity<?> cadastraClienteAsaas =
+                asaasProxy.cadastraCliente(clienteSistemaRequest, headers);
+
+        ClienteSistemaResponse clienteSistemaResponse =
+                objectMapper.convertValue(cadastraClienteAsaas.getBody(), ClienteSistemaResponse.class);
+
+        clienteSistema.setCodigoClienteAsaas(clienteSistemaResponse.getId());
+
+        System.err.println(cadastraClienteAsaas);
+        System.err.println(clienteSistemaResponse);
+
+        return clienteSistemaRepositoryImpl.implementaPersistencia(clienteSistema);
+
+    }
+
 
 }
