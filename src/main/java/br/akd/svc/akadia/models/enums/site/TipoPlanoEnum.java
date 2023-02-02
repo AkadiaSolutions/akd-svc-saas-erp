8dba5d3b082d21f4c34951e05a125c9393902e71
Gabriel <gabriellagrota23@gmail.com>
2023-02-02 08:49:00 -0300

    feat: TipoPlanoEnum agora possui atributo qtdLimiteEmpresasCadastradas

--- src/main/java/br/akd/svc/akadia/models/enums/site/TipoPlanoEnum.java
+++ src/main/java/br/akd/svc/akadia/models/enums/site/TipoPlanoEnum.java
@@ -7,11 +7,13 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum TipoPlanoEnum {
 
-    BASIC (0, "Basic"),
-    PRO (1, "Pro"),
-    CUSTOM (2, "Custom");
+    BASIC (0, "Basic", 250.0, 1),
+    STANDART (1, "Standart", 400.0, 3),
+    PRO (2, "Pro", 650.00, 5);
 
     private final int code;
     private final String desc;
+    private final Double valor;
+    private final Integer qtdLimiteEmpresasCadastradas;
 
 }
