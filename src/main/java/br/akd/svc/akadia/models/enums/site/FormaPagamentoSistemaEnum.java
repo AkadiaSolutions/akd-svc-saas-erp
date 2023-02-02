5c4d18f7d7f923ac1219dbe0a0d5e15f919a8816
Gabriel <gabriellagrota23@gmail.com>
2023-02-02 11:58:17 -0300

    feat: Constante do Enum FormaPagamentoSistemaEnum CARTAO_CREDITO alterada para CREDIT_CARD para atender aos requisitos da integradora

--- src/main/java/br/akd/svc/akadia/models/enums/site/FormaPagamentoSistemaEnum.java
+++ src/main/java/br/akd/svc/akadia/models/enums/site/FormaPagamentoSistemaEnum.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum FormaPagamentoSistemaEnum {
 
     BOLETO (0, "Boleto"),
-    CARTAO_CREDITO (1, "Cartão de crédito"),
+    CREDIT_CARD(1, "Cartão de crédito"),
     PIX (2, "Pix");
 
     private final int code;
