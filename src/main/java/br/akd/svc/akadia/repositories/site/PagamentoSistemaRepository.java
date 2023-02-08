663dcc7b8a8f1b77fcbcc5c83dc60d654b93965f
Gabriel <gabriellagrota23@gmail.com>
2023-02-08 01:53:05 -0300

    feat: Criado método de busca por código de pagamento asaas no banco de dados

--- src/main/java/br/akd/svc/akadia/repositories/site/PagamentoSistemaRepository.java
+++ src/main/java/br/akd/svc/akadia/repositories/site/PagamentoSistemaRepository.java
@@ -4,5 +4,11 @@ import br.akd.svc.akadia.models.entities.site.PagamentoSistemaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface PagamentoSistemaRepository extends JpaRepository<PagamentoSistemaEntity, Long> {}
+public interface PagamentoSistemaRepository extends JpaRepository<PagamentoSistemaEntity, Long> {
+
+    Optional<PagamentoSistemaEntity> findByCodigoPagamentoAsaas(String codigoPagamentoAsaas);
+
+}
--- src/main/java/br/akd/svc/akadia/repositories/site/impl/PagamentoSistemaRepositoryImpl.java
+++ src/main/java/br/akd/svc/akadia/repositories/site/impl/PagamentoSistemaRepositoryImpl.java
@@ -1,13 +1,28 @@
 package br.akd.svc.akadia.repositories.site.impl;
 
+import br.akd.svc.akadia.models.entities.site.PagamentoSistemaEntity;
 import br.akd.svc.akadia.repositories.site.PagamentoSistemaRepository;
+import br.akd.svc.akadia.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PagamentoSistemaRepositoryImpl {
 
     @Autowired
     PagamentoSistemaRepository pagamentoSistemaRepository;
+
+    public PagamentoSistemaEntity implementaBuscaPorCodigoClienteAsaas(String codigoPagamentoAsaas) {
+        Optional<PagamentoSistemaEntity> pagamentoOptional =
+                pagamentoSistemaRepository.findByCodigoPagamentoAsaas(codigoPagamentoAsaas);
+
+        PagamentoSistemaEntity pagamento;
+        if (pagamentoOptional.isPresent()) pagamento = pagamentoOptional.get();
+        else throw new ObjectNotFoundException("Nenhum pagamento foi encontrado com o codigo Asaas informado");
+
+        return pagamento;
+    }
 
 }
