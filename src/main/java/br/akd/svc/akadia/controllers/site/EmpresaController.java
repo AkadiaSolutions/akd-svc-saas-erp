6ea72f72113e791c67c1568f9cc1eea54532163f
Gabriel <gabriellagrota23@gmail.com>
2023-02-09 18:19:22 -0300

    feat: Criado método controlador para visualização de todas as empresas salvas e método controlador para update das empresas

--- src/main/java/br/akd/svc/akadia/controllers/site/EmpresaController.java
+++ src/main/java/br/akd/svc/akadia/controllers/site/EmpresaController.java
@@ -2,6 +2,8 @@ package br.akd.svc.akadia.controllers.site;
 
 import br.akd.svc.akadia.models.dto.site.EmpresaDto;
 import br.akd.svc.akadia.models.entities.site.ClienteSistemaEntity;
+import br.akd.svc.akadia.models.entities.site.EmpresaEntity;
+import br.akd.svc.akadia.repositories.site.impl.EmpresaRepositoryImpl;
 import br.akd.svc.akadia.services.exceptions.InvalidRequestException;
 import br.akd.svc.akadia.services.site.EmpresaService;
 import io.swagger.annotations.Api;
@@ -9,12 +11,14 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @RestController
 @Api(value = "Essa API disponibiliza os endpoints de CRUD da entidade Empresa")
@@ -25,6 +29,14 @@ public class EmpresaController {
 
     @Autowired
     EmpresaService empresaService;
+
+    @Autowired
+    EmpresaRepositoryImpl empresaRepositoryImpl;
+
+    @GetMapping
+    public ResponseEntity<List<EmpresaEntity>> listaTodasEmpresas() {
+        return ResponseEntity.status(HttpStatus.OK).body(empresaRepositoryImpl.buscaTodasEmpresas());
+    }
 
     @ApiOperation(
             value = "Criação",
@@ -37,10 +49,27 @@ public class EmpresaController {
             @ApiResponse(code = 400, message = "Falha na criação da empresa", response = InvalidRequestException.class),
             @ApiResponse(code = 401, message = "Falha de autenticação"),
     })
-    @PostMapping(value = "cria-nova/{idCliente}")
+    @PostMapping(value = "{idCliente}/cria-nova")
     public ResponseEntity<ClienteSistemaEntity> criaEmpresa(@PathVariable Long idCliente,
                                                             @RequestBody EmpresaDto empresaDto) {
-        return ResponseEntity.ok().body(empresaService.criaNovaEmpresa(idCliente, empresaDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(empresaService.criaNovaEmpresa(idCliente, empresaDto));
+    }
+
+    @ApiOperation(
+            value = "Criação",
+            notes = "Essa requisição tem como objetivo realizar a persistência de uma nova empresa no banco de dados",
+            produces = MediaType.APPLICATION_JSON,
+            consumes = MediaType.APPLICATION_JSON
+    )
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Empresa atualizada com sucesso", response = EmpresaDto.class),
+            @ApiResponse(code = 400, message = "Falha na atualização da empresa", response = InvalidRequestException.class),
+            @ApiResponse(code = 401, message = "Falha de autenticação"),
+    })
+    @PutMapping(value = "atualiza/{idEmpresa}")
+    public ResponseEntity<EmpresaEntity> atualizaEmpresa(@PathVariable Long idEmpresa,
+                                                         @RequestBody EmpresaDto empresaDto) {
+        return ResponseEntity.ok().body(empresaService.atualizaEmpresa(idEmpresa, empresaDto));
     }
 
 }
