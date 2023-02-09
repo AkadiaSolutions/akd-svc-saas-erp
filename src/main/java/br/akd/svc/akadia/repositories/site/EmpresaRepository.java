4db209bcce63bcf9394310ded5e092d5f45c11f5
Gabriel <gabriellagrota23@gmail.com>
2023-02-09 15:23:25 -0300

    feat: Criadas validações para busca de empresa por cnpj e endpoint

--- src/main/java/br/akd/svc/akadia/repositories/site/EmpresaRepository.java
+++ src/main/java/br/akd/svc/akadia/repositories/site/EmpresaRepository.java
@@ -4,5 +4,12 @@ import br.akd.svc.akadia.models.entities.site.EmpresaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface EmpresaRepository extends JpaRepository<EmpresaEntity, Long> {}
+public interface EmpresaRepository extends JpaRepository<EmpresaEntity, Long> {
+
+    Optional<EmpresaEntity> findByCnpj(String cnpj);
+    Optional<EmpresaEntity> findByEndpoint(String endpoint);
+
+}
--- src/main/java/br/akd/svc/akadia/repositories/site/impl/EmpresaRepositoryImpl.java
+++ src/main/java/br/akd/svc/akadia/repositories/site/impl/EmpresaRepositoryImpl.java
@@ -1,13 +1,24 @@
 package br.akd.svc.akadia.repositories.site.impl;
 
+import br.akd.svc.akadia.models.entities.site.EmpresaEntity;
 import br.akd.svc.akadia.repositories.site.EmpresaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmpresaRepositoryImpl {
 
     @Autowired
     EmpresaRepository empresaRepository;
+
+    public Optional<EmpresaEntity> implementaBuscaPorCnpj(String cnpj) {
+        return empresaRepository.findByCnpj(cnpj);
+    }
+
+    public Optional<EmpresaEntity> implementaBuscaPorEndpoint(String endpoint) {
+        return empresaRepository.findByEndpoint(endpoint);
+    }
 
 }
