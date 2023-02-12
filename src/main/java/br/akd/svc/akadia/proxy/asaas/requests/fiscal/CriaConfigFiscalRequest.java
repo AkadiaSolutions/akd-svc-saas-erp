b42b10a5ba2e09fb76ff4b166669850b5ba662bc
Gabriel <gabriellagrota23@gmail.com>
2023-02-11 23:50:23 -0300

    feat: Criados models para configuração fiscal das assinaturas

---
+++ src/main/java/br/akd/svc/akadia/proxy/asaas/requests/fiscal/CriaConfigFiscalRequest.java
@@ -0,0 +1,25 @@
+package br.akd.svc.akadia.proxy.asaas.requests.fiscal;
+
+import br.akd.svc.akadia.proxy.asaas.global.Taxes;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class CriaConfigFiscalRequest {
+
+    private String municipalServiceId;
+    private String municipalServiceCode;
+    private String municipalServiceName;
+    private String updatePayment;
+    private Integer deductions;
+    private EffectiveDatePeriodEnum effectiveDatePeriod;
+    private Boolean receivedOnly;
+    private Integer daysBeforeDueDate;
+    private String observations;
+    private Taxes taxes;
+
+}
