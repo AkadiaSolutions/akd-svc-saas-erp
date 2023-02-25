f653db2be649ddb1e7ecc540ee040c41452b1962
Gabriel <gabriellagrota23@gmail.com>
2023-02-24 23:23:34 -0300

    config: Endpoints do Swagger ajustados

--- src/main/java/br/akd/svc/akadia/config/SwaggerConfig.java
+++ src/main/java/br/akd/svc/akadia/config/SwaggerConfig.java
@@ -22,7 +22,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.akd.svc.akadia"))
-                .paths(regex("/api/v1.*"))
+                .paths(regex("/.*"))
                 .build()
                 .apiInfo(metaInfo());
     }
