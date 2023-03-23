c2a86d82f8d6e8fab553b40dfde050ef3cbb3fe8
Gabriel <gabriellagrota23@gmail.com>
2023-03-23 15:18:52 -0300

    feat: Objeto ClienteResponse criado. Seu objetivo é abstrair informações desnecessárias, aumentando drasticamente a segurança de dados

---
+++ src/main/java/br/akd/svc/akadia/models/dto/sistema/clientes/responses/ClienteResponse.java
@@ -0,0 +1,32 @@
+package br.akd.svc.akadia.models.dto.sistema.clientes.responses;
+
+import br.akd.svc.akadia.models.entities.global.EnderecoEntity;
+import br.akd.svc.akadia.models.entities.global.TelefoneEntity;
+import br.akd.svc.akadia.models.entities.sistema.clientes.ExclusaoClienteEntity;
+import br.akd.svc.akadia.models.enums.sistema.clientes.StatusClienteEnum;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteResponse {
+
+    private Long id;
+    private String dataCadastro;
+    private String horaCadastro;
+    private String dataNascimento;
+    private String nome;
+    private String cpfCnpj;
+    private String inscricaoEstadual;
+    private String email;
+    private StatusClienteEnum statusCliente;
+    private Integer qtdOrdensRealizadas;
+    private Double giroTotal;
+    private ExclusaoClienteEntity exclusaoCliente;
+    private EnderecoEntity endereco;
+    private TelefoneEntity telefone;
+    private String nomeColaboradorResponsavel;
+}
